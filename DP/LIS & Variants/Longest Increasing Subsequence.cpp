6ff4ce43279c16f7fb663a344a3b02b7ccd72dbe
codebysantosh <santosh.prajapati6225@gmail.com>
2025-07-19 22:16:52 +0530

    Update Longest Increasing Subsequence.cpp
    Solution for Longest Increasing Subsequence with 3 Approaches

--- DP/LIS & Variants/Longest Increasing Subsequence.cpp
+++ DP/LIS & Variants/Longest Increasing Subsequence.cpp
@@ -49,3 +49,31 @@ public:
         return maxL;
     }
 };
+
+//Approach-3 (Using concept of Patience Sorting (O(nlogn))
+//T.C : O(nlogn)
+//S.C : O(n)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> sorted;
+        
+        for(int i = 0; i<n; i++) {
+            /*
+			     Why lower bound ?
+				 We want increasing subsequence and  hence 
+				 we want to eliminate the duplicates as well.
+				 lower_bound returns iterator to "next greater or equal to"
+			*/
+            auto it = lower_bound(begin(sorted), end(sorted), nums[i]);
+            
+            if(it == end(sorted))
+                sorted.push_back(nums[i]); //greatest : so insert it
+            else
+                *it = nums[i]; //replace
+        }
+        
+        return (int)sorted.size();
+    }
+};
