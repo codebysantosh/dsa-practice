67a493ed1e7b0b8fa1e27b706fadba232d5b3c00
codebysantosh <santosh.prajapati6225@gmail.com>
2025-07-25 00:46:09 +0530

    Create BFS_Traversal.cpp
    Solution for BFS Traversal

---
+++ Graph/BFS_Traversal.cpp
@@ -0,0 +1,41 @@
+//TC = O(V+E)
+//SC = O(V)
+class Solution {
+  public:
+    void BFS(vector<vector<int>> &adj, int u, vector<bool> &visited, vector<int> &result) {
+        queue<int> q;
+        q.push(u);
+        visited[u] = true;
+        result.push_back(u);
+        
+        
+        while(!q.empty()) {
+            
+            int u = q.front();
+            q.pop();
+            
+            for(auto v : adj[u]) {
+                if(!visited[v]) {
+                    q.push(v);
+                    visited[v] = true;
+                    result.push_back(v);
+                }
+            }   
+        }
+    }
+  
+    vector<int> bfs(vector<vector<int>> &adj) {
+        int n = adj.size();
+        
+        vector<bool> visited(n);
+        vector<int> result;
+        
+        for(int i = 0; i<n; i++) {
+            if(!visited[i]) {
+                BFS(adj, i, visited, result);
+            }
+        }
+        
+        return result;
+    }
+};
