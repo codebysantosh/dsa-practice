53a073bb537ca6178f5ba111f91a95c8692fda27
codebysantosh <santosh.prajapati6225@gmail.com>
2025-08-04 23:33:15 +0530

    Update Binary Search Tree to Greater Sum Tree.cpp

--- Tree/BST/Binary Search Tree to Greater Sum Tree.cpp
+++ Tree/BST/Binary Search Tree to Greater Sum Tree.cpp
@@ -1,4 +1,52 @@
-//Simple recursive approach
+//Approach - I (BF)
+//T.C = (n*n)
+//S.C = (n)
+class Solution {
+public:
+    vector<int> sorted;
+
+    void dfs(TreeNode* root) {
+        if(root == NULL) {
+            return;
+        }
+
+        dfs(root->left);
+        sorted.push_back(root->val);
+        dfs(root->right);
+    }
+
+    int getSum(int val) {
+        int n = sorted.size();
+
+        int sum = 0;
+        for(int i = n-1; i>=0; i--) {
+            sum += sorted[i];
+            if(sorted[i] == val) break;
+        }
+
+        return sum;
+    }
+
+    void gst(TreeNode* root) {
+        if(root == NULL) {
+            return;
+        }
+
+        root->val = getSum(root->val);
+
+        gst(root->left);
+        gst(root->right);
+    }
+
+    TreeNode* bstToGst(TreeNode* root) {
+        dfs(root);
+        gst(root);
+
+        return root;
+    }
+};
+
+//Simple recursive approach (op)
 //T.C : O(n)
 //S.C : O(1)
 class Solution {
