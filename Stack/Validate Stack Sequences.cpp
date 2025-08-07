2fa5314ca4302f219f7f2453503fee60a5cab1a3
codebysantosh <santosh.prajapati6225@gmail.com>
2025-08-07 23:04:44 +0530

    Create Validate Stack Sequences.cpp

---
+++ Stack/Validate Stack Sequences.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int n = pushed.size();
+        stack<int> st;
+
+        int i = 0, j = 0;
+        while(i < n && j < n) {
+
+            st.push(pushed[i]);
+
+            while(!st.empty() && j < n && st.top() == popped[j]) {
+                st.pop();
+                j++;
+            }
+            i++;
+        }
+
+        return st.empty();
+    }
+};
