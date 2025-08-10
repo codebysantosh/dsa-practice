125b3ccf8bf7058022ace20a1f5f564d69dd5258
codebysantosh <santosh.prajapati6225@gmail.com>
2025-08-11 01:08:04 +0530

    Create Convert 1D Array Into 2D Array.cpp

---
+++ Arrays/Convert 1D Array Into 2D Array.cpp
@@ -0,0 +1,26 @@
+//T.C = O(m*n)
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if(m*n != original.size()) {
+            return {};
+        }
+
+        vector<vector<int>> result(m, vector<int> (n));
+
+        int k = 0;
+        for(int i = 0; i<m; i++) {
+
+            for(int j = 0; j<n; j++) {
+
+                if(k < original.size()) {
+                    result[i][j] = original[k];
+                    k++;
+                }
+
+            }
+        }
+
+        return result;
+    }
+};
