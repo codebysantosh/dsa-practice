c1f5aaf922a854b8a3346d5863b9f4c4f6056243
codebysantosh <santosh.prajapati6225@gmail.com>
2025-08-11 14:23:30 +0530

    Create Container With Most Water.cpp

---
+++ Arrays/Two Pointer/Container With Most Water.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+
+        int maxArea = 0;
+
+        int i = 0, j = n-1;
+        while(i < j) {
+
+            int w = j - i;
+            int area = w * min(height[i], height[j]);
+            maxArea = max(maxArea, area);
+
+            if(height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        return maxArea;
+    }
+};
