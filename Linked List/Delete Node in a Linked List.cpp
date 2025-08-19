aa022ddda3e0b85479af8798e8b78acca328d37e
codebysantosh <santosh.prajapati6225@gmail.com>
2025-08-19 22:39:14 +0530

    Create Delete Node in a Linked List.cpp

---
+++ Linked List/Delete Node in a Linked List.cpp
@@ -0,0 +1,19 @@
+//T.C = O(n)
+//S.C = O(1)
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* prev = NULL;
+        
+        while(node != NULL && node->next != NULL) {
+            node->val = node->next->val;
+
+            prev = node;
+
+            node = node->next;
+        }
+
+        prev->next = NULL;
+        delete(node);
+    }
+};
