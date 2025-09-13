013100c0389c004917ec66f4bbe7d7dfec9c9b8b
codebysantosh <santosh.prajapati6225@gmail.com>
2025-09-13 22:47:25 +0530

    Create Remove Element.cpp

---
+++ Arrays/Two Pointer/Remove Element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0, j = nums.size()-1;
+
+        while(i <= j) {
+            if(nums[i] == val && nums[j] != val) {
+                swap(nums[i], nums[j]);
+                i++; 
+                j--;
+            } else if(nums[i] == val && nums[j] == val){
+                j--;
+            } else {
+                i++;
+            }
+        }
+
+        // cout<<i<<endl;
+
+        return i;
+    }
+};
